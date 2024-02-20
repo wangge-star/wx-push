7260b07544f10f56b6339debff53945a58ffaf2c
wangge-star <85119761+wangge-star@users.noreply.github.com>
2024-02-21 00:58:15 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   template_id: "LX7GKPkMT_zf19qS93G1ee6nMVGXYgO_6I22U9nURAQ",
   touser: "oCKKC6d7fRqIOpKg9L9ImHoS-BjI",
 
-  key: "201c93c2e2b34bdeecdbf9f5265eb84e",
+  key: "oCKKC6d7fRqIOpKg9L9ImHoS-BjI",
   city: "邢台",
   first_date: "2022-11-10",
 };
